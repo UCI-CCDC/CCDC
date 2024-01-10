025515c494f27eaae7181dc58d8970c0562d61fc
Dhruv Kandula <dkandula@uci.edu>
2024-01-10 14:26:29 -0800

    add nano to normalize

--- linux-dev/linux-toolbox/normalize.sh
+++ linux-dev/linux-toolbox/normalize.sh
@@ -6,21 +6,21 @@ command_exists() {
 echo "Installing essential packages:"
 if command_exists apt-get; then
     apt update -y
-    apt-get install -y coreutils net-tools iproute2 iptables bash curl git net-tools vim wget grep tar jq gpg
+    apt-get install -y coreutils net-tools iproute2 iptables bash curl git net-tools vim wget grep tar jq gpg nano
 fi
 
 if command_exists yum; then
     yum check-update -y
-    yum install -y bash coreutils net-tools iproute2 iptables bash curl git net-tools vim wget grep tar jq gpg
+    yum install -y bash coreutils net-tools iproute2 iptables bash curl git net-tools vim wget grep tar jq gpg nano
 fi
 
 if command_exists pacman; then
     pacman -Syu --noconfirm
-    pacman -S --noconfirm coreutils net-tools iproute2 iptables bash curl git net-tools vim wget grep tar jq gpg
+    pacman -S --noconfirm coreutils net-tools iproute2 iptables bash curl git net-tools vim wget grep tar jq gpg nano
 fi
 
 if command_exists apk; then
     apk update --no-confirm
-    apk add coreutils net-tools iproute2 iptables bash curl git net-tools vim wget grep tar jq gpg
+    apk add coreutils net-tools iproute2 iptables bash curl git net-tools vim wget grep tar jq gpg nano
 fi
 echo "Essential packages stage done."
