e4d458da64c226527b726c5ef970acb67931f89e
Charles Wu <charlw5@uci.edu>
2024-01-13 19:57:29 -0800

    https://genius.com/17237820

---
+++ linux-dev/linux-hardening/key_perms.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+sep () {
+    echo "======================================================================================================="
+}
+
+dash_sep () {
+    echo "-------------------------------------------------------------------------------------------------------"
+}
+
+command_exists() {
+  command -v "$1" &> /dev/null
+}
+
+if [ "$(id -u)" -ne 0 ]; then
+    printf 'Must be run as root, exiting!\n'
+    exit 1
+fi
+
+if ! command_exists awk; then
+    echo "Command awk not found. Normalize the machine!"
+    exit 1
+fi
+sep
+echo "Hardening SSH keys..."
+sep
+
+# Update permissions for root's SSH keys
+if [ -d "/root/.ssh" ]; then
+    for file in "/root/.ssh/id_"*; do
+        [ -f "$file" ] && chmod 600 "$file" && echo "Found and changed $file." 2>/dev/null || echo "$file not found for root."
+    done
+    echo "[+] All root id keys locked down."
+    dash_sep
+else
+    echo "[-] SSH directory not found for root."
+    dash_sep
+fi
+
+awk -F: '($3 >= 1000) && $1 != "nobody" {print $1}' /etc/passwd | while read -r user; do
+    if [ -d "/home/$user/.ssh" ]; then
+	for file in "/home/$user/.ssh/id_"*; do
+            [ -f "$file" ] && chmod 600 "$file" && echo "Found and changed $file." 2>/dev/null || echo "$file not found for user $user."
+        done
+        echo "[+] All id keys for user $user locked down."
+    	dash_sep
+    else
+	
+        echo "[-] SSH directory not found for user $user."
+	dash_sep
+    fi
+done
+
+echo "...SSH keys that were found have updated permissions."
+
