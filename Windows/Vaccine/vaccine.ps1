a70c214ff68bf7639c8088cdccf8e1e5534d5c7d
joshchung02 <joshchung02@gmail.com>
2024-01-16 20:02:11 -0800

    vaccine init, some organization

--- Windows/Vaccine/vaccine.ps1
+++ Windows/Vaccine/vaccine.ps1
@@ -1,58 +1,62 @@
-REM  Run this script in an elevated CMD prompt
-REM  This does not include firewall rules
-REM Comment out any sections that do not apply to a certain service (ex. SMB scored box)
-REM Don't forget to use msconfig
+<#
+Previously named security.bat, upgraded to be a PowerShell script
+#>
+
+# REM  Run this script in an elevated CMD prompt
+# REM  This does not include firewall rules
+# REM Comment out any sections that do not apply to a certain service (ex. SMB scored box)
+# REM Don't forget to use msconfig
 
 
-REM sc.exe config “wfcs” displayname= “wfcservice”
-REM sc.exe description wfcs “wfcservice description”
-REM perm charlie delta
-@echo off
+# REM sc.exe config “wfcs” displayname= “wfcservice”
+# REM sc.exe description wfcs “wfcservice description”
+# REM perm charlie delta
+# @echo off
 
-REM rename admin acct + disable default accounts
-REM wmic useraccount where "name='Administrator'" rename Admin
-REM net user Administrator /active:no
+# REM rename admin acct + disable default accounts
+# REM wmic useraccount where "name='Administrator'" rename Admin
+# REM net user Administrator /active:no
 net user Guest /active:no
 net user DefaultAccount /active:no
 net user WDAGUtilityAccount /active:no
 
 
-REM Enable full auditing
-auditpol /set /category:* /success:enable /failure:enable
+# REM Enable full auditing
+# auditpol /set /category:* /success:enable /failure:enable
 
 
-REM Disable Admin Shares (psexec defense)
+# REM Disable Admin Shares (psexec defense)
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" /v AutoShareWks /t REG_DWORD /d 0 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" /v AutoShareServer /t REG_DWORD /d 0 /f
 reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System" /v LocalAccountTokenFilterPolicy /d 0 /f
 
 
-REM SMBv1 Disable 
+# REM SMBv1 Disable 
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Services\LanmanServer\Parameters" /v SMB1 /t REG_DWORD /d 1 /f
 
 
-REM Hashing
+# REM Hashing
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v NoLMHash /t REG_DWORD /d 1 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v LMCompatibilityLevel /t REG_DWORD /d 5 /f
 
 
-REM Anon Login
+# REM Anon Login
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v restrictanonymous /t REG_DWORD /d 1 /f
 reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v disableRestrictedAdmin /t REG_DWORD /d 0 /f 
 
 
-REM Disable Keys
+# REM Disable Keys
 reg add "HKCU\Control Panel\Accessibility\StickyKeys" /v Flags /t REG_SZ /d 506 /f
 reg add "HKCU\Control Panel\Accessibility\ToggleKeys" /v Flags /t REG_SZ /d 58 /f
 reg add "HKCU\Control Panel\Accessibility\Keyboard Response" /v Flags /t REG_SZ /d 122 /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI" /v ShowTabletKeyboard /t REG_DWORD /d 0 /f
 
 
-REM disable floppy disk remoting
+# REM disable floppy disk remoting
 reg ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AllocateFloppies /t REG_DWORD /d 1 /f
 
 
-REM Enable SMB Signing (prevent smb ntlm relaying attacks)
+# REM Enable SMB Signing (prevent smb ntlm relaying attacks)
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Services\LanmanServer\Parameters" /v EnableSecuritySignature /t REG_DWORD /d 1 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Services\LanmanServer\Parameters" /v RequireSecuritySignature  /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Rdr\Parameters" /v EnableSecuritySignature /t REG_DWORD /d 1 /f
@@ -60,63 +64,63 @@ reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Rdr\Parameters" /v
 
 
 
-REM Prevent print driver installs 
+# REM Prevent print driver installs 
 reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Print\Providers\LanMan Print Services\Servers" /v AddPrinterDrivers /t REG_DWORD /d 1 /f
 
 
-REM local account blank passwords
+# REM local account blank passwords
 reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v LimitBlankPasswordUse /t REG_DWORD /d 1 /f
 
 
-REM  enable full uac
+# REM  enable full uac
 reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v PromptOnSecureDesktop /t REG_DWORD /d 1 /f
 
 
-REM Enable installer detections
+# REM Enable installer detections
 reg ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableInstallerDetection /t REG_DWORD /d 1 /f
 
 
-REM anon enumeration prevention
+# REM anon enumeration prevention
 reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymous /t REG_DWORD /d 1 /f 
 reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v restrictanonymoussam /t REG_DWORD /d 1 /f 
 
 
-REM domain cred storing 
+# REM domain cred storing 
 reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v disabledomaincreds /t REG_DWORD /d 1 /f 
 
-REM no perms to anons
+# REM no perms to anons
 reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Lsa /v everyoneincludesanonymous /t REG_DWORD /d 0 /f 
 
 
-REM smb strengtheners
+# REM smb strengtheners
 reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanWorkstation\Parameters /v EnablePlainTextPassword /t REG_DWORD /d 0 /f
 reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionPipes /t REG_MULTI_SZ /d "" /f
 reg ADD HKLM\SYSTEM\CurrentControlSet\services\LanmanServer\Parameters /v NullSessionShares /t REG_MULTI_SZ /d "" /f
 
 
-REM remote registry path denial
+# REM remote registry path denial
 reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedExactPaths /v Machine /t REG_MULTI_SZ /d "" /f
 reg ADD HKLM\SYSTEM\CurrentControlSet\Control\SecurePipeServers\winreg\AllowedPaths /v Machine /t REG_MULTI_SZ /d "" /f
 
 
-REM Require UAC
+# REM Require UAC
 reg add HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 1 /f
 reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\Installer /v AlwaysInstallElevated /t REG_DWORD /d 0 /f
 
 
-REM  Enable LSASS Memory Protection
+# REM  Enable LSASS Memory Protection
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Lsa" /v RunAsPPL /t REG_DWORD /d 1 /f
 
 
-REM Enable Credential Guard
+# REM Enable Credential Guard
 reg add "HKLM\SYSTEM\CurrentControl\Control\Lsa" /v LsaCfgFlags /t REG_DWORD /d 1 /f
 
 
-REM Disable plain text passwords stored in LSASS
+# REM Disable plain text passwords stored in LSASS
 reg add HKLM\SYSTEM\CurrentControlSet\Control\SecurityProviders\WDigest /v UseLogonCredential /t REG_DWORD /d 0 /f 
 
 
-REM Enable PowerShell Logging
+# REM Enable PowerShell Logging
 reg add "HKLM\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\PowerShell\ModuleLogging" /v EnableModuleLogging /t REG_DWORD /d 1 /f
 reg add "HKLM\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging" /v EnableScriptBlockLogging /t REG_DWORD /d 1 /f
 
