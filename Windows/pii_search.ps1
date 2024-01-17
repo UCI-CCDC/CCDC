7e04001892cf90275d0792411ff7a8596efc66c3
joshchung02 <joshchung02@gmail.com>
2024-01-16 22:28:02 -0800

    filter for extensions, address regex

--- Windows/pii_search.ps1
+++ Windows/pii_search.ps1
@@ -9,15 +9,18 @@ param(
 $ErrorActionPreference = "SilentlyContinue"
 
 $patterns = 
-    '\d{3}[)]?[-| |.]\d{3}[-| |.]\d{4}', 
-    '\d{3}[-| |.]\d{2}[-| |.]\d{4}'
+    '\b\d{3}[)]?[-| |.]\d{3}[-| |.]\d{4}\b', 
+    '\b\d{3}[-| |.]\d{2}[-| |.]\d{4}\b',
+    '\b\d+\s+[\w\s]+\s+(?:road|street|avenue|boulevard|court)\b'
+$fileExtensions = "\.docx|\.doc|\.odt|\.xlsx|\.xls|\.ods|\.pptx|\.ppt|\.odp|\.pdf|\.mdb|\.accdb|\.sqlite3?|\.eml|\.msg|\.txt|\.csv|\.html?|\.xml"
 
-Get-ChildItem -Recurse -Force -Path $Path | ?{ findstr.exe /mprc:. $_.FullName } | foreach {
+Get-ChildItem -Recurse -Force -Path $Path | ?{$_.Extension -match $fileExtensions} | foreach {
     if ($pii = Select-String -Path $_.FullName -Pattern $patterns) {
         "PII found in $($_.FullName)"
 	
         if (!$Silent) {
-            $pii
+            $pii | select -ExpandProperty Matches | Sort-Object Value -Unique | select Value -ExpandProperty Value
         }
+        Write-Output ""
     }
 }
