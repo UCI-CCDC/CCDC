d419cee207a54161cc83c97e43a1460c8c93ee59
joshchung02 <joshchung02@gmail.com>
2024-01-16 22:32:33 -0800

    woops forgot json

--- Windows/pii_search.ps1
+++ Windows/pii_search.ps1
@@ -12,7 +12,7 @@ $patterns =
     '\b\d{3}[)]?[-| |.]\d{3}[-| |.]\d{4}\b', 
     '\b\d{3}[-| |.]\d{2}[-| |.]\d{4}\b',
     '\b\d+\s+[\w\s]+\s+(?:road|street|avenue|boulevard|court)\b'
-$fileExtensions = "\.docx|\.doc|\.odt|\.xlsx|\.xls|\.ods|\.pptx|\.ppt|\.odp|\.pdf|\.mdb|\.accdb|\.sqlite3?|\.eml|\.msg|\.txt|\.csv|\.html?|\.xml"
+$fileExtensions = "\.docx|\.doc|\.odt|\.xlsx|\.xls|\.ods|\.pptx|\.ppt|\.odp|\.pdf|\.mdb|\.accdb|\.sqlite3?|\.eml|\.msg|\.txt|\.csv|\.html?|\.xml|\.json"
 
 Get-ChildItem -Recurse -Force -Path $Path | ?{$_.Extension -match $fileExtensions} | foreach {
     if ($pii = Select-String -Path $_.FullName -Pattern $patterns) {
