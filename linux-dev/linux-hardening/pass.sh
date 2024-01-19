e235c4178307ed51dde19a6c1ba6c786f3812cf6
Akshay Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>
2024-01-18 17:10:22 -0800

    yung amogus

--- linux-dev/linux-hardening/pass.sh
+++ linux-dev/linux-hardening/pass.sh
@@ -5,10 +5,9 @@ while IFS=: read -r username _ uid _; do
         newpass=$(tr -dc 'a-zA-Z0-9' </dev/urandom | head -c "15") # make a random password
 	    echo "$username,$newpass"
         if command -v chpasswd >/dev/null 2>&1; then
-            echo "$username:$password" | chpasswd
+            echo "$username:$newpass" | chpasswd
         else
-            printf "%s\n%s\n" "$password" "$password" | passwd "$username"
+            printf "%s\n%s\n" "$newpass" "$newpass" | passwd "$username"
         fi
-
     fi
 done < /etc/passwd
