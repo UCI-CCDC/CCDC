233e67fd0a3e71c6f5f5bd90869ec2164c2d31b6
Akshay Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>
2024-03-23 11:48:24 -0700

    rm bashisms + user-defined path

--- linux-dev/linux-inventory/k7esbackup.sh
+++ linux-dev/linux-inventory/k7esbackup.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-keywords=("deployments" "statefulsets" "services" "ingress" "pv" "pvc" "serviceaccounts")
+keywords="deployments statefulsets services ingress pv pvc"
 
-for type in "${keywords[@]}"; do
+for type in $keywords; do
     output=$(kubectl get $type --all-namespaces | awk 'NR>1 {print $1, $2}')
     if [ "$output" = "" ]; then
         echo "No $type"
@@ -10,18 +10,18 @@ for type in "${keywords[@]}"; do
     else
         while IFS= read -r line; do
             if [ "$type" = "pv" ]; then
-                mkdir -p /opt/k8sbak/$type
+                mkdir -p $1$type
                 name=$(echo "$line" | awk '{print $1}')
                 echo "Processing $type $name"
-                kubectl get $type $name -o yaml > "/opt/k8sbak/$type/$name.yaml"
+                kubectl get $type $name -o yaml > "$backup_path$type/$name.yaml"
             else
                 namespace=$(echo "$line" | awk '{print $1}')
                 name=$(echo "$line" | awk '{print $2}')
                 echo "Processing $type $name in namespace $namespace"
-                mkdir -p /opt/k8sbak/$namespace/$type
-                kubectl -n $namespace get $type $name -o yaml > "/opt/k8sbak/$namespace/$type/$name.yaml"
+                mkdir -p "$1$namespace/$type"
+                kubectl -n $namespace get $type $name -o yaml > "$1$namespace/$type/$name.yaml"
             fi
         done <<< "$output"
     fi
 done
-echo "Backups made in /opt/k8sbak"
+echo "Backups made in $1"
