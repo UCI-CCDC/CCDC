3ae3c32a269aeb73ecedc33b26f363bf9d624a90
Akshay Rohatgi <52616034+Akshay-Rohatgi@users.noreply.github.com>
2024-04-18 19:13:06 -0700

    CAL POLY POMON(OV)A

--- linux-dev/linux-hardening/blue.sh
+++ linux-dev/linux-hardening/blue.sh
@@ -18,22 +18,22 @@ else
     cp /etc/passwd /opt/passwd.bak
     chmod 644 /opt/passwd.bak
 
-    if ! which rbash >/dev/null ; then
+    if ! which rbash 1> /dev/null 2>& 1 ; then
         ln -sf /bin/bash /bin/rbash
     fi
 
-    if command -v bash >/dev/null ; then
+    if command -v bash 1> /dev/null 2>& 1 ; then
         head -1 /etc/passwd > /etc/pw
         sed -n '1!p' /etc/passwd | sed 's/\/bin\/.*sh$/\/bin\/rbash/g' >> /etc/pw
         mv /etc/pw /etc/passwd
         chmod 644 /etc/passwd
     fi
 
-    for homedir in /home/* ; do 
-        echo 'PATH=""' >> $homedir/.*shrc 
-        echo 'export PATH' >> $homedir/.*shrc
+    for file in $(find /etc /home -name *.*shrc -exec ls {} \;); do
+        echo 'PATH=""' >> $file
+        echo 'export PATH' >> $file
         if command -v apk >/dev/null; then
-            echo 'export PATH' >> $homedir/.profile
+            echo 'export PATH' >> $file
         fi
     done
 
