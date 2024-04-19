c28ef4767388c43b1d2855004a9284d97feb5e29
Dhruv Kandula <dkandula@uci.edu>
2024-04-18 18:33:29 -0700

    db backups

---
+++ linux-dev/linux-toolbox/db/mysql_back.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <mysql_host> <mysql_user> <mysql_password> <path>"
+    exit 1
+fi
+
+
+sep () {
+    echo "======================================================================================================="
+}
+
+dash_sep () {
+    echo "-------------------------------------------------------------------------------------------------------"
+}
+
+
+MYSQL_HOST="$1"
+MYSQL_USER="$2"
+MYSQL_PASSWORD="$3"
+DATA_PATH="$4"
+
+mkdir $DATA_PATH
+cd $DATA_PATH
+
+
+
+echo "MySQL backups"
+sep
+
+
+databases=`mysql -h $MYSQL_HOST -u $MYSQL_USER --password=$MYSQL_PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+echo "$databases"
+for db in $databases; do
+    if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
+        echo "Dumping database: $db"
+        mysqldump -h $MYSQL_HOST -u $MYSQL_USER --password=$MYSQL_PASSWORD --databases $db > $db.sql
+       # gzip $OUTPUT/`date +%Y%m%d`.$db.sql
+    fi
+done
+
+
+
+echo "MySQL backups Finished"
---
+++ linux-dev/linux-toolbox/db/psql_back.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <psql host> <psql_user> <psql_password> <data_path>"
+    exit 1
+fi
+
+
+PSQL_HOST="$1"
+PSQL_USER="$2"
+export PGPASSWORD="$3"
+DATA_PATH="$4"
+echo "PSQL Auditing"
+
+
+mkdir $DATA_PATH
+cd $DATA_PATH
+
+# List all databases
+psql -h "$PSQL_HOST" -U "$PSQL_USER" -t -c "SELECT datname FROM pg_database;" > database_list.txt
+let numlines="$(wc -l < database_list.txt) - 1"
+head -n $numlines database_list.txt > tmp.txt; mv tmp.txt database_list.txt
+
+echo "List of Databases:"
+cat database_list.txt
+
+while read selected_db; do
+    pg_dump -h "$PSQL_HOST" -U "$PSQL_USER" -d $selected_db > "$selected_db.sql"
+done <database_list.txt
+
+echo "PSQL Auditing finished"
